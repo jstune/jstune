98542d497e16dd5d19d7b0b8f681bba86e8a2207
Vue Play <author@vueplay.com>
2024-03-05 15:58:12 +0100

    Stored by Vue Play

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,14 +3,16 @@
 		<WrapperPage class="justify-center">
 			<div style="max-width:100%;width:240px">
 				<h2 class="mb-2">
-					Login
+					JSTune
 				</h2><input
 					class="mb-3 font-extralight text-slate-800 py-2.5 px-2 rounded-md w-full"
 					placeholder="Username"
 				/><input
 					class="font-extralight text-slate-800 py-2.5 px-2 rounded-md w-full"
 					placeholder="Password"
-				/><button class="hover:bg-fuchsia-950 mt-4 rounded-lg bg-fuchsia-900 w-full p-3">Button</button>
+				/><button class="hover:bg-fuchsia-950 mt-4 rounded-lg bg-fuchsia-900 w-full p-3">
+					Login
+				</button>
 			</div>
 		</WrapperPage>
 	</div>
