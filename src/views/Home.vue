dd5904ba7a43760178fbe948e3a9f118dc9306dd
Vue Play <author@vueplay.com>
2024-03-05 16:42:53 +0100

    Stored by Vue Play

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
 	<TemplateHero title="Applications">
 		<WrapperPage>
-			<div class="w-full flex-col my-16 py-4 flex">
+			<div class="w-full flex-col my-16 py-4 px-6 flex">
 				<h1 class="mb-8 font-semibold text-4xl">
 					Next generation front end tooling
 				</h1>
@@ -11,51 +11,90 @@
 			</div>
 		</WrapperPage>
 		<div class="py-16 bg-slate-100 mt-6">
-			<WrapperPage>
-				<div class="grid-cols-1 w-full gap-12 sm:grid-cols-3 grid p-6">
-					<div class="drop-shadow duration-300 transition-colors hover:bg-slate-300 cursor-pointer items-center flex-col py-8 rounded-lg flex bg-slate-200">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 24 24"
-							fill="currentColor"
-							class="text-slate-400 w-16 h-16"
-						>
-							<path d="M6 12a.75.75 0 01-.75-.75v-7.5a.75.75 0 111.5 0v7.5A.75.75 0 016 12zM18 12a.75.75 0 01-.75-.75v-7.5a.75.75 0 011.5 0v7.5A.75.75 0 0118 12zM6.75 20.25v-1.5a.75.75 0 00-1.5 0v1.5a.75.75 0 001.5 0zM18.75 18.75v1.5a.75.75 0 01-1.5 0v-1.5a.75.75 0 011.5 0zM12.75 5.25v-1.5a.75.75 0 00-1.5 0v1.5a.75.75 0 001.5 0zM12 21a.75.75 0 01-.75-.75v-7.5a.75.75 0 011.5 0v7.5A.75.75 0 0112 21zM3.75 15a2.25 2.25 0 104.5 0 2.25 2.25 0 00-4.5 0zM12 11.25a2.25 2.25 0 110-4.5 2.25 2.25 0 010 4.5zM15.75 15a2.25 2.25 0 104.5 0 2.25 2.25 0 00-4.5 0z" />
-						</svg><span class="text-2xl pt-7 text-slate-800">
-							Full Control
-						</span>
-					</div>
-					<div class="drop-shadow duration-300 transition-colors hover:bg-slate-300 cursor-pointer items-center flex-col py-8 rounded-lg flex bg-slate-200">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 24 24"
-							fill="currentColor"
-							class="text-slate-400 w-16 h-16"
-						>
-							<path
-								fill-rule="evenodd"
-								d="M12 1.5a.75.75 0 01.75.75V4.5a.75.75 0 01-1.5 0V2.25A.75.75 0 0112 1.5zM5.636 4.136a.75.75 0 011.06 0l1.592 1.591a.75.75 0 01-1.061 1.06l-1.591-1.59a.75.75 0 010-1.061zm12.728 0a.75.75 0 010 1.06l-1.591 1.592a.75.75 0 01-1.06-1.061l1.59-1.591a.75.75 0 011.061 0zm-6.816 4.496a.75.75 0 01.82.311l5.228 7.917a.75.75 0 01-.777 1.148l-2.097-.43 1.045 3.9a.75.75 0 01-1.45.388l-1.044-3.899-1.601 1.42a.75.75 0 01-1.247-.606l.569-9.47a.75.75 0 01.554-.68zM3 10.5a.75.75 0 01.75-.75H6a.75.75 0 010 1.5H3.75A.75.75 0 013 10.5zm14.25 0a.75.75 0 01.75-.75h2.25a.75.75 0 010 1.5H18a.75.75 0 01-.75-.75zm-8.962 3.712a.75.75 0 010 1.061l-1.591 1.591a.75.75 0 11-1.061-1.06l1.591-1.592a.75.75 0 011.06 0z"
-								clip-rule="evenodd"
-							/>
-						</svg><span class="text-2xl pt-7 text-slate-800">
-							Click & Deploy
-						</span>
-					</div>
-					<div class="drop-shadow duration-300 transition-colors hover:bg-slate-300 cursor-pointer items-center flex-col py-8 rounded-lg flex bg-slate-200">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 24 24"
-							fill="currentColor"
-							class="text-slate-400 w-16 h-16"
-						>
-							<path
-								fill-rule="evenodd"
-								d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-2.625 6c-.54 0-.828.419-.936.634a1.96 1.96 0 00-.189.866c0 .298.059.605.189.866.108.215.395.634.936.634.54 0 .828-.419.936-.634.13-.26.189-.568.189-.866 0-.298-.059-.605-.189-.866-.108-.215-.395-.634-.936-.634zm4.314.634c.108-.215.395-.634.936-.634.54 0 .828.419.936.634.13.26.189.568.189.866 0 .298-.059.605-.189.866-.108.215-.395.634-.936.634-.54 0-.828-.419-.936-.634a1.96 1.96 0 01-.189-.866c0-.298.059-.605.189-.866zm2.023 6.828a.75.75 0 10-1.06-1.06 3.75 3.75 0 01-5.304 0 .75.75 0 00-1.06 1.06 5.25 5.25 0 007.424 0z"
-								clip-rule="evenodd"
-							/>
-						</svg><span class="text-2xl pt-7 text-slate-800">
-							Have Fun!
-						</span>
+			<WrapperPage class="flex-col">
+				<div class="w-full flex-col my-16 py-4 px-6 flex">
+					<h1 class="mb-8 font-semibold text-4xl">
+						Main Server Node
+					</h1>
+					<div class="relative rounded-xl overflow-auto max-w-sm">
+						<div class="shadow-sm overflow-hidden my-8">
+							<table class="border-collapse table-auto w-full text-sm">
+								<tbody class="bg-white dark:bg-slate-800">
+									<tr>
+										<td class="font-bold border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
+											IP
+										</td>
+										<td class="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400">
+											34.88.86.116
+										</td>
+									</tr>
+									<tr>
+										<td class="font-bold border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
+											Hostname
+										</td>
+										<td class="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400">
+											internal.host.name
+										</td>
+									</tr>
+									<tr>
+										<td class="font-bold border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
+											State
+										</td>
+										<td class="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400">
+											Ready
+										</td>
+									</tr>
+									<tr>
+										<td class="font-bold border-b border-slate-200 dark:border-slate-600 p-4 pl-8 text-slate-500 dark:text-slate-400">
+											Status
+										</td>
+										<td class="border-b border-slate-200 dark:border-slate-600 p-4 pr-8 text-slate-500 dark:text-slate-400">
+											Active
+										</td>
+									</tr>
+									<tr>
+										<td class="font-bold border-b border-slate-200 dark:border-slate-600 p-4 pl-8 text-slate-500 dark:text-slate-400">
+											RAM
+										</td>
+										<td class="border-b border-slate-200 dark:border-slate-600 p-4 pr-8 text-slate-500 dark:text-slate-400">
+											3.84GB
+										</td>
+									</tr>
+									<tr>
+										<td class="font-bold border-b border-slate-200 dark:border-slate-600 p-4 pl-8 text-slate-500 dark:text-slate-400">
+											CPU
+										</td>
+										<td class="border-b border-slate-200 dark:border-slate-600 p-4 pr-8 text-slate-500 dark:text-slate-400">
+											2 cores
+										</td>
+									</tr>
+									<tr>
+										<td class="font-bold border-b border-slate-200 dark:border-slate-600 p-4 pl-8 text-slate-500 dark:text-slate-400">
+											OS
+										</td>
+										<td class="border-b border-slate-200 dark:border-slate-600 p-4 pr-8 text-slate-500 dark:text-slate-400">
+											Linux
+										</td>
+									</tr>
+									<tr>
+										<td class="font-bold border-b border-slate-200 dark:border-slate-600 p-4 pl-8 text-slate-500 dark:text-slate-400">
+											Architecture
+										</td>
+										<td class="border-b border-slate-200 dark:border-slate-600 p-4 pr-8 text-slate-500 dark:text-slate-400">
+											x86_64
+										</td>
+									</tr>
+									<tr>
+										<td class="font-bold border-b border-slate-200 dark:border-slate-600 p-4 pl-8 text-slate-500 dark:text-slate-400">
+											Docker Version
+										</td>
+										<td class="border-b border-slate-200 dark:border-slate-600 p-4 pr-8 text-slate-500 dark:text-slate-400">
+											24.0.6
+										</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
 					</div>
 				</div>
 			</WrapperPage>
