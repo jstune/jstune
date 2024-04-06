85863497a222089e1581b5b337dc5acdf2f1c8a8
Vue Play <author@vueplay.com>
2024-04-07 00:34:32 +0200

    Stored by Vue Play

--- src/views/SetupDocker.vue
+++ src/views/SetupDocker.vue
@@ -39,7 +39,16 @@
 			title="Install using already running docker instance"
 		>
 			Install manually
+		</button><button
+			@click="connect"
+			class="hover:bg-fuchsia-950 mt-4 rounded-lg bg-fuchsia-900 w-full p-3"
+			title="Install using already running docker instance"
+		>
+			Try to reconnect
 		</button>
+		<h2 class="mt-2">
+			Platform: {{platform}} Ready: {{ready}}
+		</h2>
 	</TemplateSetup>
 </template>
 <script>
@@ -57,30 +66,45 @@
 				password: ''
 			},
 			platform: '',
-			sudo: ''
+			ready: false,
+			sudo: '',
+			loading: false
 		}),
 		async created() {
-			const result = await this.io.service('setup')
-				.get('docker');
-			this.config = result.config;
-			this.platform = result.platform;
-			console.log('result', result);
+			await this.connect();
 		},
 		methods: {
+			async connect() {
+				this.loading = true;
+				const result = await this.io.service('setup')
+					.get('docker');
+				this.config = result.config;
+				this.platform = result.platform;
+				this.ready = result.ready;
+				console.log('result', result);
+				this.loading = false;
+			},
 			async autoSetup() {
+				this.loading = true;
 				let result = await this.io.service('setup')
 					.patch('docker', {
 						sudo: this.sudo
 					});
 				this.resolve(result);
+				this.loading = false;
 			},
 			async setup() {
+				this.loading = true;
 				let result = await this.io.service('setup')
 					.patch('docker', this.config);
 				this.resolve(result);
+				this.loading = false;
 			},
 			resolve(result) {
 				console.log('result', result);
+				this.config = result.config;
+				this.platform = result.platform;
+				this.ready = result.ready;
 			}
 		}
 	};
