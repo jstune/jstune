066e78c08be7193a5531c935bd3c16b37aa72ad0
Vue Play <author@vueplay.com>
2024-04-07 15:54:13 +0200

    Stored by Vue Play

--- src/views/SetupDatabase.vue
+++ src/views/SetupDatabase.vue
@@ -1,28 +1,92 @@
 <template>
 	<TemplateSetup>
 		<h2 class="mb-2">
-			Setup Database
-		</h2><input
+			Connect to database
+		</h2> <input
+			v-model="config.host"
 			class="mb-3 font-extralight text-slate-800 py-2.5 px-2 rounded-md w-full"
-			placeholder="E-mail"
-		/><input
+			placeholder="Host: 127.0.0.1"
+		/> <input
+			v-model="config.port"
 			class="mb-3 font-extralight text-slate-800 py-2.5 px-2 rounded-md w-full"
-			placeholder="Username"
-		/><input
-			class="font-extralight text-slate-800 py-2.5 px-2 rounded-md w-full"
-			placeholder="Password"
-		/><button class="hover:bg-fuchsia-950 mt-4 rounded-lg bg-fuchsia-900 w-full p-3">
-			Install
+			placeholder="Port: 2375"
+		/> <input
+			v-model="config.user"
+			class="mb-3 font-extralight text-slate-800 py-2.5 px-2 rounded-md w-full"
+			placeholder="Username:"
+		/> <input
+			v-model="config.password"
+			class="mb-3 font-extralight text-slate-800 py-2.5 px-2 rounded-md w-full"
+			placeholder="Password:"
+		/> <input
+			v-model="config.database"
+			class="mb-3 font-extralight text-slate-800 py-2.5 px-2 rounded-md w-full"
+			placeholder="Database:"
+		/> <button
+			@click="setup()"
+			:disabled="loading"
+			class="hover:bg-fuchsia-950 mt-2 rounded-lg bg-fuchsia-900 w-full p-3"
+			title="Install using already running database instance"
+		>
+			Connect to existing database
+		</button> <button
+			@click="setup(true)"
+			:disabled="loading"
+			class="hover:bg-fuchsia-950 mt-4 rounded-lg bg-fuchsia-900 w-full p-3"
+			title="Install using already running database instance"
+		>
+			Create a new database
 		</button>
+		<h2 class="mt-2">
+			Loading: {{loading}}
+		</h2>
 	</TemplateSetup>
 </template>
 <script>
 	import TemplateSetup from '@/components/TemplateSetup.vue';
 	export default {
+		inject: ['io'],
 		components: {
 			TemplateSetup: TemplateSetup
 		},
-		data: () => ({})
+		data: () => ({
+			config: {
+				host: '',
+				port: '',
+				user: '',
+				password: '',
+				database: ''
+			},
+			loading: false
+		}),
+		async created() {
+			await this.connect();
+		},
+		methods: {
+			async connect() {
+				this.loading = true;
+				const result = await this.io.service('setup')
+					.get('database');
+				this.resolve(result);
+			},
+			async setup(create = false) {
+				this.loading = true;
+				let result = await this.io.service('setup')
+					.patch('database', {
+						...this.config,
+						create
+					});
+				this.resolve(result);
+			},
+			async resolve(result) {
+				if (result) {
+					this.config = result.config;
+				} else {
+					this.$router.push('/login');
+				}
+				this.loading = false;
+			}
+		}
 	};
 
 </script>
