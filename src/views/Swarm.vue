39715417bc5d1311766e32868b6be6f9aa4ed2a7
Vue Play <author@vueplay.com>
2024-04-19 17:11:33 +0200

    Stored by Vue Play

--- src/views/Swarm.vue
+++ src/views/Swarm.vue
@@ -1,212 +1,108 @@
 <template>
-	<TemplateHero title="Swarm Nodes">
-		<WrapperPage class="max-w-screen-2xl p-6">
-			<div class="relative rounded-xl overflow-auto w-full">
-				<div class="shadow-sm my-8">
+	<TemplateHero title="Swarm">
+		<WrapperPage class="p-6">
+			<div class="relative overflow-auto w-full">
+				<button
+					class="rounded p-2 bg-slate-200"
+					@click="getItems"
+				>
+					Reload
+				</button>
+				<div class="shadow-sm overflow-hidden my-8">
+
 					<table class="border-collapse table-auto w-full text-sm">
 						<thead>
 							<tr>
-								<th class="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
-									ID
-								</th>
-								<th class="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-center">
-									IP
-								</th>
-								<th class="text-center border-b dark:border-slate-600 font-medium p-4 pt-0 pb-3 text-slate-400 dark:text-slate-200">
+								<th class="whitespace-nowrap border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
 									Hostname
 								</th>
-								<th class="text-center border-b dark:border-slate-600 font-medium p-4 pt-0 pb-3 text-slate-400 dark:text-slate-200">
-									Type
-								</th>
-								<th class="text-center border-b dark:border-slate-600 font-medium p-4 pt-0 pb-3 text-slate-400 dark:text-slate-200">
-									RAM
-								</th>
-								<th class="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-center">
-									CPU
-								</th>
-								<th class="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-center">
-									State
-								</th>
-								<th class="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-center">
+								<th class="whitespace-nowrap text-center border-b dark:border-slate-600 font-medium p-4 pt-0 pb-3 text-slate-400 dark:text-slate-200">
 									Status
 								</th>
-								<th class="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-center">
-									OS
+								<th class="whitespace-nowrap text-center border-b dark:border-slate-600 font-medium p-4 pt-0 pb-3 text-slate-400 dark:text-slate-200">
+									Role
 								</th>
-								<th
-									class="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-center"
-									title="Architecture"
-								>
-									Architecture
+								<th class="whitespace-nowrap text-center border-b dark:border-slate-600 font-medium p-4 pt-0 pb-3 text-slate-400 dark:text-slate-200">
+									Ready
 								</th>
 								<th class="whitespace-nowrap border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-center">
-									Docker Version
+									Address
+								</th>
+								<th class="whitespace-nowrap border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-center">
+									Id
 								</th>
 								<th class="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-center">
-									Terminal
+									Created
+								</th>
+								<th class="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-center">
+									Open
 								</th>
 							</tr>
 						</thead>
 						<tbody class="bg-white dark:bg-slate-800">
-							<tr>
-								<td class="text-ellipsis overflow-hidden border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
-									nvnrs3xl9jwkquh9nroux7v73
+							<tr v-for="item in items">
+								<td class="hover:text-slate-700 cursor-pointer border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
+									{{ item.Description.Hostname }}
 								</td>
-								<td class="hover:text-slate-700 cursor-pointer border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400 text-center">
-									34.88.86.116
+								<td class="border-b border-slate-100 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400">
+									{{ item.ManagerStatus.Reachability }} {{ item.ManagerStatus.Leader ? '(Leader)' : '' }}
 								</td>
-								<td class="border-b border-slate-100 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400 text-center">
-									internal.host.name
+								<td class="border-b border-slate-100 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400">
+									{{ item.Spec.Role }} {{ item.Spec.Availability }}
 								</td>
-								<td class="border-b border-slate-100 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400 text-center">
-									Leader
-								</td>
-								<td class="text-center border-b border-slate-100 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400">
-									3.84GB
-								</td>
-								<td class="whitespace-nowrap border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400 text-center">
-									2 cores
-								</td>
-								<td class="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400 text-center">
-									ready
-								</td>
-								<td class="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400 text-center">
-									active
-								</td>
-								<td class="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400 text-center">
-									linux
-								</td>
-								<td class="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400 text-center">
-									x86_64
-								</td>
-								<td class="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400 text-center">
-									24.0.6
-								</td>
-								<td class="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400">
-									<svg
+								<td class="border-b border-slate-100 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400"><svg
 										xmlns="http://www.w3.org/2000/svg"
 										viewBox="0 0 24 24"
 										fill="currentColor"
-										class="w-6 h-6 mx-auto cursor-pointer hover:text-slate-700"
+										class="mx-auto w-6 h-6"
+										v-if="item.Status.State !== 'ready'"
 									>
 										<path
 											fill-rule="evenodd"
-											d="M2.25 6a3 3 0 013-3h13.5a3 3 0 013 3v12a3 3 0 01-3 3H5.25a3 3 0 01-3-3V6zm3.97.97a.75.75 0 011.06 0l2.25 2.25a.75.75 0 010 1.06l-2.25 2.25a.75.75 0 01-1.06-1.06l1.72-1.72-1.72-1.72a.75.75 0 010-1.06zm4.28 4.28a.75.75 0 000 1.5h3a.75.75 0 000-1.5h-3z"
+											d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z"
 											clip-rule="evenodd"
 										/>
-									</svg>
+									</svg> <svg
+										xmlns="http://www.w3.org/2000/svg"
+										viewBox="0 0 24 24"
+										fill="currentColor"
+										class="mx-auto w-6 h-6"
+										v-if="item.Status.State === 'ready'"
+									>
+										<path
+											fill-rule="evenodd"
+											d="M19.916 4.626a.75.75 0 01.208 1.04l-9 13.5a.75.75 0 01-1.154.114l-6-6a.75.75 0 011.06-1.06l5.353 5.353 8.493-12.739a.75.75 0 011.04-.208z"
+											clip-rule="evenodd"
+										/>
+									</svg> </td>
+								<td class="whitespace-nowrap border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400 text-center">
+									{{ item.ManagerStatus.Addr }}
 								</td>
+								<td class="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400 text-center">
+									<input v-model="item.ID" />
+								</td>
+								<td class="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400 text-center">
+									{{ item.CreatedAt }}
+
+								</td>
+								<td class="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400"><svg
+										xmlns="http://www.w3.org/2000/svg"
+										viewBox="0 0 24 24"
+										fill="currentColor"
+										class="hover:text-slate-700 cursor-pointer w-6 h-6 mx-auto"
+									>
+										<path
+											fill-rule="evenodd"
+											d="M15.75 2.25H21a.75.75 0 01.75.75v5.25a.75.75 0 01-1.5 0V4.81L8.03 17.03a.75.75 0 01-1.06-1.06L19.19 3.75h-3.44a.75.75 0 010-1.5zm-10.5 4.5a1.5 1.5 0 00-1.5 1.5v10.5a1.5 1.5 0 001.5 1.5h10.5a1.5 1.5 0 001.5-1.5V10.5a.75.75 0 011.5 0v8.25a3 3 0 01-3 3H5.25a3 3 0 01-3-3V8.25a3 3 0 013-3h8.25a.75.75 0 010 1.5H5.25z"
+											clip-rule="evenodd"
+										/>
+									</svg> </td>
 							</tr>
 						</tbody>
 					</table>
 				</div>
 			</div>
 		</WrapperPage>
-		<div class="py-16 bg-slate-100 mt-6">
-			<WrapperPage class="flex-col">
-				<div class="w-full flex-col my-16 py-4 px-6 flex">
-					<h1 class="mb-8 font-semibold text-4xl">
-						Main Server Node
-					</h1>
-					<div class="relative rounded-xl overflow-auto max-w-md">
-						<div class="shadow-sm overflow-hidden my-8">
-							<table class="border-collapse table-auto w-full text-sm">
-								<tbody class="bg-white dark:bg-slate-800">
-									<tr>
-										<td class="font-bold border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
-											IP
-										</td>
-										<td class="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400">
-											34.88.86.116
-										</td>
-									</tr>
-									<tr>
-										<td class="font-bold border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
-											Hostname
-										</td>
-										<td class="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400">
-											internal.host.name
-										</td>
-									</tr>
-									<tr>
-										<td class="font-bold border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
-											State
-										</td>
-										<td class="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400">
-											Ready
-										</td>
-									</tr>
-									<tr>
-										<td class="font-bold border-b border-slate-200 dark:border-slate-600 p-4 pl-8 text-slate-500 dark:text-slate-400">
-											Status
-										</td>
-										<td class="border-b border-slate-200 dark:border-slate-600 p-4 pr-8 text-slate-500 dark:text-slate-400">
-											Active
-										</td>
-									</tr>
-									<tr>
-										<td class="font-bold border-b border-slate-200 dark:border-slate-600 p-4 pl-8 text-slate-500 dark:text-slate-400">
-											RAM
-										</td>
-										<td class="border-b border-slate-200 dark:border-slate-600 p-4 pr-8 text-slate-500 dark:text-slate-400">
-											1.24GB of 3.84GB used
-										</td>
-									</tr>
-									<tr>
-										<td class="font-bold border-b border-slate-200 dark:border-slate-600 p-4 pl-8 text-slate-500 dark:text-slate-400">
-											Storage
-										</td>
-										<td class="border-b border-slate-200 dark:border-slate-600 p-4 pr-8 text-slate-500 dark:text-slate-400">
-											12GB of 100GB used
-										</td>
-									</tr>
-									<tr>
-										<td class="font-bold border-b border-slate-200 dark:border-slate-600 p-4 pl-8 text-slate-500 dark:text-slate-400">
-											CPU
-										</td>
-										<td class="border-b border-slate-200 dark:border-slate-600 p-4 pr-8 text-slate-500 dark:text-slate-400">
-											2 cores
-										</td>
-									</tr>
-									<tr>
-										<td class="font-bold border-b border-slate-200 dark:border-slate-600 p-4 pl-8 text-slate-500 dark:text-slate-400">
-											OS
-										</td>
-										<td class="border-b border-slate-200 dark:border-slate-600 p-4 pr-8 text-slate-500 dark:text-slate-400">
-											Linux
-										</td>
-									</tr>
-									<tr>
-										<td class="font-bold border-b border-slate-200 dark:border-slate-600 p-4 pl-8 text-slate-500 dark:text-slate-400">
-											Architecture
-										</td>
-										<td class="border-b border-slate-200 dark:border-slate-600 p-4 pr-8 text-slate-500 dark:text-slate-400">
-											x86_64
-										</td>
-									</tr>
-									<tr>
-										<td class="font-bold border-b border-slate-200 dark:border-slate-600 p-4 pl-8 text-slate-500 dark:text-slate-400">
-											Docker Version
-										</td>
-										<td class="border-b border-slate-200 dark:border-slate-600 p-4 pr-8 text-slate-500 dark:text-slate-400">
-											24.0.6
-										</td>
-									</tr>
-									<tr>
-										<td class="font-bold border-b border-slate-200 dark:border-slate-600 p-4 pl-8 text-slate-500 dark:text-slate-400">
-											Additional Nodes
-										</td>
-										<td class="border-b border-slate-200 dark:border-slate-600 p-4 pr-8 text-slate-500 dark:text-slate-400">
-											3
-										</td>
-									</tr>
-								</tbody>
-							</table>
-						</div>
-					</div>
-				</div>
-			</WrapperPage>
-		</div>
 	</TemplateHero>
 </template>
 <script>
@@ -217,8 +113,33 @@
 			WrapperPage,
 			TemplateHero
 		},
-		inject: ['menus'],
-		data: () => ({})
+		inject: ['menus', 'io'],
+		data: () => ({
+			service: 'swarm',
+			items: null
+		}),
+		async created() {
+			await this.getItems();
+		},
+		methods: {
+			async getItems() {
+				try {
+					this.items = await this.io.service(this.service)
+						.find({
+							query: {
+								limit: 100,
+								since: '',
+								// node ID or name
+								before: '' // node ID or name
+							}
+						});
+					console.log(this.items);
+				} catch (e) {
+					this.items = null;
+					console.error(e);
+				}
+			}
+		}
 	};
 
 </script>
