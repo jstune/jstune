77d65fb9002a8fa3ddca6c41e324e1cdeb5c84cc
Vue Play <author@vueplay.com>
2024-04-19 17:12:44 +0200

    Stored by Vue Play

--- src/views/Swarm.vue
+++ src/views/Swarm.vue
@@ -35,7 +35,7 @@
 									Created
 								</th>
 								<th class="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-center">
-									Open
+									Termi
 								</th>
 							</tr>
 						</thead>
@@ -89,11 +89,11 @@
 										xmlns="http://www.w3.org/2000/svg"
 										viewBox="0 0 24 24"
 										fill="currentColor"
-										class="hover:text-slate-700 cursor-pointer w-6 h-6 mx-auto"
+										class="w-6 h-6 mx-auto cursor-pointer hover:text-slate-700"
 									>
 										<path
 											fill-rule="evenodd"
-											d="M15.75 2.25H21a.75.75 0 01.75.75v5.25a.75.75 0 01-1.5 0V4.81L8.03 17.03a.75.75 0 01-1.06-1.06L19.19 3.75h-3.44a.75.75 0 010-1.5zm-10.5 4.5a1.5 1.5 0 00-1.5 1.5v10.5a1.5 1.5 0 001.5 1.5h10.5a1.5 1.5 0 001.5-1.5V10.5a.75.75 0 011.5 0v8.25a3 3 0 01-3 3H5.25a3 3 0 01-3-3V8.25a3 3 0 013-3h8.25a.75.75 0 010 1.5H5.25z"
+											d="M2.25 6a3 3 0 013-3h13.5a3 3 0 013 3v12a3 3 0 01-3 3H5.25a3 3 0 01-3-3V6zm3.97.97a.75.75 0 011.06 0l2.25 2.25a.75.75 0 010 1.06l-2.25 2.25a.75.75 0 01-1.06-1.06l1.72-1.72-1.72-1.72a.75.75 0 010-1.06zm4.28 4.28a.75.75 0 000 1.5h3a.75.75 0 000-1.5h-3z"
 											clip-rule="evenodd"
 										/>
 									</svg> </td>
