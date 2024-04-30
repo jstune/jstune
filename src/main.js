b1580a80150b4608b67b17909c0a17521337e3e4
Vue Play <author@vueplay.com>
2024-04-30 11:52:52 +0200

    Stored by Vue Play

--- src/main.js
+++ src/main.js
@@ -33,7 +33,10 @@ io.io.on('connect', () => {
 });
 
 let user = ref()
-try { user.value = (await io.reAuthenticate())?.user } catch(e) {}
+
+io.reAuthenticate().then(user => {
+    user.value = user
+}).catch(e => {})
 
 router.beforeEach(async to => {
     if (to.path === '/logout') {
