d40d2517b1d57efeb5e4298a2f711e03458f0d49
Vue Play <author@vueplay.com>
2024-05-10 16:29:51 +0200

    Stored by Vue Play

---
+++ src/views/ResetPassword.vue
@@ -0,0 +1,36 @@
+<template>
+	<TemplateSetup>
+		<h2 class="mb-2">
+			JSTune
+		</h2><input
+			class="mb-3 font-extralight text-slate-800 py-2.5 px-2 rounded-md w-full"
+			placeholder="E-mail / Username"
+		/><input
+			class="mb-3 font-extralight text-slate-800 py-2.5 px-2 rounded-md w-full"
+			placeholder="Password"
+		/><button class="hover:bg-fuchsia-950 mt-4 rounded-lg bg-fuchsia-900 w-full p-3">
+			Recover
+		</button><router-link
+			to="/login"
+			class="hover:text-slate-200 mt-6 inline-block text-center w-full"
+		>
+			Go to login
+		</router-link><router-link
+			to="/register"
+			class="mt-3 hover:text-slate-200 inline-block text-center w-full"
+		>
+			Request access
+		</router-link>
+	</TemplateSetup>
+</template>
+<script>
+	import TemplateSetup from '@/components/TemplateSetup.vue';
+	export default {
+		components: {
+			TemplateSetup: TemplateSetup
+		},
+		data: () => ({})
+	};
+
+</script>
+<style scoped></style>
