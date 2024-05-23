a9d976503354d1aa31738c303c786980e699b647
Vue Play <author@vueplay.com>
2024-05-23 12:27:07 +0200

    Stored by Vue Play

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -207,6 +207,45 @@
 							<h2 class="text-slate-100">
 								Environment
 							</h2>
+						</router-link><router-link
+							to="/env"
+							class="bg-slate-700 from-slate-800 aspect-video shadow-md rounded-lg items-center justify-center flex-col bg-gradient-to-tr flex cursor-pointer hover:opacity-80"
+						>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 24 24"
+								fill="currentColor"
+								class="text-white w-12 h-12"
+							>
+								<path d="M19.5 6h-15v9h15V6z" />
+								<path
+									fill-rule="evenodd"
+									d="M3.375 3C2.339 3 1.5 3.84 1.5 4.875v11.25C1.5 17.16 2.34 18 3.375 18H9.75v1.5H6A.75.75 0 006 21h12a.75.75 0 000-1.5h-3.75V18h6.375c1.035 0 1.875-.84 1.875-1.875V4.875C22.5 3.839 21.66 3 20.625 3H3.375zm0 13.5h17.25a.375.375 0 00.375-.375V4.875a.375.375 0 00-.375-.375H3.375A.375.375 0 003 4.875v11.25c0 .207.168.375.375.375z"
+									clip-rule="evenodd"
+								/>
+							</svg>
+							<h2 class="text-slate-100">
+								Monitor
+							</h2>
+						</router-link><router-link
+							to="/env"
+							class="bg-slate-700 from-slate-800 aspect-video shadow-md rounded-lg items-center justify-center flex-col bg-gradient-to-tr flex cursor-pointer hover:opacity-80"
+						>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 24 24"
+								fill="currentColor"
+								class="text-white w-12 h-12"
+							>
+								<path
+									fill-rule="evenodd"
+									d="M2.25 6a3 3 0 013-3h13.5a3 3 0 013 3v12a3 3 0 01-3 3H5.25a3 3 0 01-3-3V6zm3.97.97a.75.75 0 011.06 0l2.25 2.25a.75.75 0 010 1.06l-2.25 2.25a.75.75 0 01-1.06-1.06l1.72-1.72-1.72-1.72a.75.75 0 010-1.06zm4.28 4.28a.75.75 0 000 1.5h3a.75.75 0 000-1.5h-3z"
+									clip-rule="evenodd"
+								/>
+							</svg>
+							<h2 class="text-slate-100">
+								Terminal
+							</h2>
 						</router-link> <router-link
 							to="/users"
 							class="bg-slate-700 from-slate-800 aspect-video shadow-md rounded-lg items-center justify-center flex-col bg-gradient-to-tr flex cursor-pointer hover:opacity-80"
