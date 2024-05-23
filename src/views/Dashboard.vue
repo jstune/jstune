fe4dcd9993edb1b52944808c0799b07293ad9f50
Vue Play <author@vueplay.com>
2024-05-23 12:23:47 +0200

    Stored by Vue Play

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -189,6 +189,24 @@
 							<h2 class="text-slate-100">
 								Authentication
 							</h2>
+						</router-link><router-link
+							to="/env"
+							class="bg-slate-700 from-slate-800 aspect-video shadow-md rounded-lg items-center justify-center flex-col bg-gradient-to-tr flex cursor-pointer hover:opacity-80"
+						><svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 24 24"
+								fill="currentColor"
+								class="text-white w-12 h-12"
+							>
+								<path
+									fill-rule="evenodd"
+									d="M11.484 2.17a.75.75 0 011.032 0 11.209 11.209 0 007.877 3.08.75.75 0 01.722.515 12.74 12.74 0 01.635 3.985c0 5.942-4.064 10.933-9.563 12.348a.749.749 0 01-.374 0C6.314 20.683 2.25 15.692 2.25 9.75c0-1.39.223-2.73.635-3.985a.75.75 0 01.722-.516l.143.001c2.996 0 5.718-1.17 7.734-3.08zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zM12 15a.75.75 0 00-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 00.75-.75v-.008a.75.75 0 00-.75-.75H12z"
+									clip-rule="evenodd"
+								/>
+							</svg>
+							<h2 class="text-slate-100">
+								Environment
+							</h2>
 						</router-link> <router-link
 							to="/users"
 							class="bg-slate-700 from-slate-800 aspect-video shadow-md rounded-lg items-center justify-center flex-col bg-gradient-to-tr flex cursor-pointer hover:opacity-80"
