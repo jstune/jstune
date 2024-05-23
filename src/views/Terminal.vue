a8fd094547dfa9a0268146a2aa04c7ba22752bbe
Vue Play <author@vueplay.com>
2024-05-23 17:57:55 +0200

    Stored by Vue Play

--- src/views/Terminal.vue
+++ src/views/Terminal.vue
@@ -16,17 +16,27 @@
 				</h1>
 				<div class="mt-6 relative mb-16 flex-col md:flex-row flex w-full">
 					<textarea
+						ref="output"
 						readonly=""
 						v-model="output"
 						class="justify-start flex-col-reverse flex overflow-auto h-64 w-full p-4 rounded bg-slate-50"
 					/>
 					<div class="-mb-6 bg-slate-100 -bottom-5 absolute h-0.5 left-2 right-2">
 					</div>
-				</div><input class="p-4 my-4 bg-slate-50 rounded" /><button
+				</div><input
+					v-model="command"
+					@keypress.enter="submit()"
+					class="p-4 my-4 bg-slate-50 rounded"
+				/><button
 					class="p-4 rounded bg-slate-200"
-					@click="update()"
+					@click="submit()"
 				>
 					Go
+				</button><button
+					class="p-4 rounded bg-slate-200 my-4"
+					@click="autoscroll = !autoscroll"
+				>
+					Autoscroll {{ autoscroll ? 'on' :'off' }}
 				</button>
 			</div>
 		</WrapperPage>
@@ -45,8 +55,10 @@
 		inject: ['io'],
 		props: ['renderer'],
 		data: () => ({
+			command: '',
 			address: '',
-			output: ''
+			output: '',
+			autoscroll: true
 		}),
 		async created() {
 			try {
@@ -56,6 +68,9 @@
 			this.io.service('terminal')
 				.on('output', line => {
 					this.output += `${line}\n`;
+					if (this.autoscroll) {
+						this.$refs.output.scrollTop = this.$refs.output.scrollHeight;
+					}
 				});
 		},
 		methods: {
@@ -77,39 +92,17 @@
 					}
 				}
 			},
-			async update() {
-				if (!confirm(`Are you sure you want to update jstune?`)) {
-					return false;
+			async submit() {
+				try {
+					const result = await this.io.service('exec')
+						.patch({
+							command: this.command
+						});
+					console.log('result', result);
+					this.command = '';
+				} catch (e) {
+					alert(e.message);
 				}
-				await this.io.service('exec')
-					.patch('update');
-				alert('jstune will automtically be restarted after update is completed');
-			},
-			async updateNPM() {
-				if (!confirm(`Are you sure you want to update packages?`)) {
-					return false;
-				}
-				await this.io.service('exec')
-					.patch('update-npm');
-				alert('jstune will automtically be restarted after update is completed');
-			},
-			async updateUI() {
-				if (!confirm(`Are you sure you want to update admin ui?`)) {
-					return false;
-				}
-				await this.io.service('exec')
-					.patch('update-ui');
-				location.reload();
-			},
-			async reboot() {
-				if (!confirm(`Are you sure you want to reboot?`)) {
-					return false;
-				}
-				await this.io.service('exec')
-					.patch('reboot');
-				setTimeout(() => {
-					location.reload();
-				}, 3000);
 			}
 		}
 	};
