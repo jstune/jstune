04d65e79e41f2b164af9afb97f15b6345a359c67
Vue Play <author@vueplay.com>
2024-05-27 15:05:48 +0200

    Stored by Vue Play

--- src/views/DNSProviders.vue
+++ src/views/DNSProviders.vue
@@ -32,7 +32,7 @@
 									Provider Key
 								</th>
 								<th class="text-center border-b dark:border-slate-600 font-medium p-4 pt-0 pb-3 text-slate-400 dark:text-slate-200">
-									Key
+									Key / Token
 								</th>
 								<th class="text-center border-b dark:border-slate-600 font-medium p-4 pt-0 pb-3 text-slate-400 dark:text-slate-200">
 									Secret
