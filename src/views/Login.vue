0a00693e38b8820b0592fc993d6716ac0d1c57a7
Vue Play <author@vueplay.com>
2024-09-03 10:40:52 +0200

    Stored by Vue Play

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -64,9 +64,6 @@
 				const path = server.endsWith('/') ? `oauth/${provider}` : `/oauth/${provider}`;
 				const redir = encodeURIComponent(`${server}${path}/callback?app_url=${location.origin}`)
 				location.href = server + path + `?redirect_uri=${redir}`;
-
-				const pathOld = this.server.endsWith('/') ? `oauth/${provider}` : `/oauth/${provider}`;
-				const locationHref = this.server + path + `?app_url=${location.origin}`;
 			},
 			async getLoginProviders() {
 				try {
